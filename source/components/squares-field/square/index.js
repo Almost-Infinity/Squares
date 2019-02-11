dde86295482daf9778ec1453f9192e3b107490fa
Alfinity <its.alfinity@gmail.com>
2019-02-11 20:52:59 +0200

    Little commit

--- source/components/squares-field/square/index.js
+++ source/components/squares-field/square/index.js
@@ -8,20 +8,17 @@ class Square extends Component {
 	}
 
 	pathGenerator(w = 128, h = 128) {
-		const lines = Math.ceil((w / 15) * 2); // 20 - расстояние между линиями
-		const offsetW = w / Math.floor(lines / 2);
-		const offsetH = h / Math.floor(lines / 2);
+		const biggestSide = Math.max(w, h),
+			lines = Math.floor(biggestSide / 15) + 1,
+			offsetW = w / lines,
+			offsetH = h / lines;
 
 		let ret = `M0 0 L${w} ${h}`;
-		for (let i = 0, ox = 0, ly = h; i < Math.floor(lines / 2); i++) {
-			ox += offsetW;
-			ly -= offsetH;
+		for (let i = 0, ox = offsetW, ly = h - offsetH; i < lines - 1; i++, ox += offsetW, ly -= offsetH) {
 			ret += ` M${ox} 0 L${w} ${ly}`;
 		}
 
-		for (let i = 0, oy = 0, lx = w - 20; i < Math.floor(lines / 2); i++) {
-			oy += offsetH;
-			lx = w - offsetW * (i + 1);
+		for (let i = 0, oy = offsetH, lx = w - offsetW; i < lines - 1; i++, oy += offsetH, lx -= offsetW) {
 			ret += ` M0 ${oy} L${lx} ${h}`;
 		}
 		return ret;
@@ -31,7 +28,7 @@ class Square extends Component {
 		return (
 			<svg className="square" version="1.1" xmlns="http://www.w3.org/2000/svg" width={ `${this.props.width}` } height={ `${this.props.height}` } style={{ margin: '10px' }}>
 				<rect width={ `${this.props.width}` } height={ `${this.props.height}` } fill="transparent" strokeWidth="2" stroke={ this.props.color } />
-				<path strokeWidth="2" stroke={ this.props.color } d={ this.pathGenerator(this.props.width, this.props.height) } />
+				<path strokeWidth="1" stroke={ this.props.color } d={ this.pathGenerator(this.props.width, this.props.height) } />
 			</svg>
 		);
 	}
