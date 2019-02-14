6485ecac917f977cf1c78358313d578325bb993e
Alfinity <its.alfinity@gmail.com>
2019-02-14 20:58:15 +0200

    Added 'global' app context

--- source/App.js
+++ source/App.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
 import Layout from './components/layout';
+import { AppContext } from './App-context';
 
 class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			wndWidth: 0,
+			wndHeight: 0,
+			isMobile: false
+		};
+
+		this.onWndResize = this.onWndResize.bind(this);
+	}
+
+	componentDidMount() {
+		this.onWndResize();
+		window.addEventListener('resize', this.onWndResize);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.onWndResize);
+	}
+
+	onWndResize() {
+		this.setState({
+			wndHeight: window.innerHeight,
+			wndWidth: window.innerWidth,
+			isMobile: (window.innerWidth <= 768) // 768 – tablet max width
+		});
+	}
+
 	render() {
-		return <Layout />;
+		return (
+			<AppContext.Provider value={ this.state }>
+				<Layout />
+			</AppContext.Provider>
+		);
 	}
 }
 
