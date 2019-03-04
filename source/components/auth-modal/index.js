94926197513e42db8854c6f48fdeb1bac42c9fc0
Alfinity <its.alfinity@gmail.com>
2019-03-04 21:38:10 +0200

    Auth modal styling

--- source/components/auth-modal/index.js
+++ source/components/auth-modal/index.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Modal from '../modal';
 import $ from 'jquery';
-import { AuthModalContext } from './context';
 import './styles.sass';
 
 class AuthModal extends Component {
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
 
 		this.state = {
 			authOrRestoreMode: true, // true - Авторизация, false - Восстановление парля
@@ -20,54 +19,65 @@ class AuthModal extends Component {
 
 	toggleMode() {
 		this.setState({ authOrRestoreMode: !this.state.authOrRestoreMode });
-		$('.auth-form-input').val('');
+		$('.auth-input').val('');
 	}
 
 	onFormSubmit(event) {
 		event.preventDefault();
 		this.setState({ inputError: '' });
 
-		$.post(this.state.authOrRestoreMode ? '/login' : '/restore-password', $('.auth-input-form').serialize(), data => {
+		$.post(this.state.authOrRestoreMode ? '/login' : '/restore-password', $('.auth-input').serialize(), data => {
 			this.setState({ inputError: data });
 		});
 	}
 
 	componentDidMount() {
-		$('.auth-input-form').bind('submit', this.onFormSubmit);
+		$('.auth-input').bind('submit', this.onFormSubmit);
 	}
 
 	componentWillUnmount() {
-		$('.auth-input-form').unbind('submit', this.onFormSubmit);
+		$('.auth-input').unbind('submit', this.onFormSubmit);
 	}
 
 	componentWillUpdate() {
-		$('.auth-input-form').unbind('submit', this.onFormSubmit);
+		$('.auth-input').unbind('submit', this.onFormSubmit);
 	}
 
 	componentDidUpdate() {
-		$('.auth-input-form').bind('submit', this.onFormSubmit);
+		$('.auth-input').bind('submit', this.onFormSubmit);
 	}
 
 	render() {
+		const { toggleAuthModal } = this.props;
+
 		const AuthComponent = (
 			<React.Fragment>
-				<form className="auth-input-form">
-					<input className="auth-form-input" placeholder="Электронная почта" name="auth-email" />
-					<input className="auth-form-input" placeholder="Пароль" name="auth-pass" />
-					<button className="auth-form-btn">Авторизироваться</button>
+				<form className="auth-form clearfix">
+					<label className="auth-input-label">
+						Электронная почта
+						<input className="auth-input" name="auth-email" />
+					</label>
+					<label className="auth-input-label">
+						Пароль
+						<a className="auth-link" href="javascript:void(0)" onClick={ this.toggleMode }>Восстановить</a>
+						<input className="auth-input" name="auth-pass" />
+					</label>
+					<label className="auth-checkbox">
+						<input type="checkbox" name="auth-remember" />
+						<span className="checkbox"></span>
+						Запомнить
+					</label>
+					<button className="auth-submit">Войти</button>
 				</form>
-				<Link className="auth-link" to="/registration" onClick={ this.context.toggleAuthModal }>Зарегистрироваться</Link>
-				<a className="auth-link" href="javascript:void(0)" onClick={ this.toggleMode }>Восстановить пароль</a>
 			</React.Fragment>
 		);
 
 		const PassRestoreComponent = (
 			<React.Fragment>
-				<form className="auth-input-form">
-					<input className="auth-form-input" placeholder="Электронная почта" name="restore-email" />
+				<form className="auth-input">
+					<input className="auth-input" placeholder="Электронная почта" name="restore-email" />
 					<button className="auth-form-btn">Отправить запрос</button>
 				</form>
-				<Link className="auth-link" to="/registration" onClick={ this.context.toggleAuthModal }>Зарегистрироваться</Link>
 				<a className="auth-link" href="javascript:void(0)" onClick={ this.toggleMode }>Авторизироваться</a>
 			</React.Fragment>
 		);
@@ -77,12 +87,14 @@ class AuthModal extends Component {
 				title={ this.state.authOrRestoreMode ? 'Авторизация' : 'Восстановление пароля' }
 				error_msg={ this.state.inputError }
 				content={ this.state.authOrRestoreMode ? AuthComponent : PassRestoreComponent }
-				close_fn={ this.context.toggleAuthModal }
+				close_fn={ toggleAuthModal }
 			/>
 		);
 	}
 }
 
-AuthModal.contextType = AuthModalContext;
+AuthModal.propTypes = {
+	toggleAuthModal: PropTypes.func.isRequired
+};
 
 export default AuthModal;
