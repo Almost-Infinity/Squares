7570e9ac02262b7425acb3c94280a08777fcfe29
Alfinity <its.alfinity@gmail.com>
2019-03-06 12:06:12 +0200

    Created

---
+++ source/components/registration/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles.sass';
+
+export default class Registration extends Component {
+	render() {
+		return (
+			<div className="registration">
+				<p>Hello, world!</p>
+			</div>
+		);
+	}
+}
