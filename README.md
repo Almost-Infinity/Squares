16443ba49cda1e0a24b84e4077bb3ad7eff94e97
Alfinity <its.alfinity@gmail.com>
2019-03-25 21:45:14 +0200

    A lot of improvements

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 Squares Game v0.2-dev
-├──────────────────────────────────────────────────────────┤
 Copyright (c) 2019 Almost Infinity a.k.a Alfinity
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
-'use strict';
-
 const path = require('path');
+const chalk = require('chalk');
 const webpack = require('webpack');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
 const isProduction = (process.env.NODE_ENV === 'production');
 
@@ -15,18 +16,22 @@ const BUILD_PATH = path.resolve(__dirname, 'build');
 module.exports = {
 	watch: !isProduction,
 	mode: process.env.NODE_ENV,
+	devtool: isProduction ? 'source-map' : 'inline-source-map',
+	stats: 'minimal',
 
 	entry: SOURCE_PATH + '/main.js',
 	output: {
 		path: BUILD_PATH,
 		filename: isProduction ? 'static/scripts/squares.[hash:8].js' : 'static/scripts/squares.js',
-		chunkFilename: isProduction ? 'static/scripts/[id].chunk.[chunkhash:8].js' : 'static/scripts/[id].chunk.js'
+		chunkFilename: isProduction ? 'static/scripts/[name].[chunkhash:8].js' : 'static/scripts/[name].chunk.js'
 	},
 
 	devServer: {
-		contentBase: BUILD_PATH,
+		contentBase: path.join(__dirname, 'build'),
+		stats: 'minimal',
+		overlay: false,
+		clientLogLevel: 'none',
 		compress: true,
-		host: '0.0.0.0',
 		port: 1337,
 		hot: true
 	},
@@ -35,16 +40,13 @@ module.exports = {
 		splitChunks: {
 			chunks: 'all'
 		},
-		runtimeChunk: true,
-		namedModules: !isProduction,
-		moduleIds: isProduction ? 'hashed' : 'named',
-		mangleWasmImports: isProduction
+		runtimeChunk: true
 	},
 
 	module: {
 		rules: [{
+			test: /\.js?$/,
 			enforce: 'pre',
-			test: /\.jsx?$/,
 			exclude: /node_modules/,
 			loader: 'eslint-loader',
 			options: {
@@ -60,75 +62,78 @@ module.exports = {
 				presets: [
 					'@babel/preset-env',
 					'@babel/preset-react'
-				],
-				plugins: [
-					'@babel/plugin-proposal-class-properties'
 				]
 			}
 		}, {
 			test: /\.(s(a|c)ss)$/,
-			use: [
-				isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-				{
-					loader: 'css-loader',
-					options: {
-						sourceMap: !isProduction
-					}
-				}, {
-					loader: 'postcss-loader',
-					options: {
-						ident: 'postcss',
-						plugins: () => [
-							require('postcss-flexbugs-fixes'),
-							require('postcss-preset-env')({
-								autoprefixer: {
-									flexbox: 'no-2009',
-								},
-								stage: 3
-							}),
-							require('css-mqpacker'),
-							require('cssnano')({
-								preset: [ 'advanced', {
-									autoprefixer: false,
-									zindex: false,
-									cssDeclarationSorter: isProduction
-								}],
-							})
-						],
-						sourceMap: !isProduction
-					}
-				}, {
-					loader: 'sass-loader',
-					options: {
-						sourceMap: !isProduction
-					}
-				}
-			]
-		}, {
-			test: /\.(png|jpeg?|svg|gif)$/,
 			use: [{
-				loader: isProduction ? 'file-loader' : 'url-loader',
+				loader: isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+			}, {
+				loader: 'css-loader',
 				options: {
-					outputPath: '/static/images/'
-				},
+					url: false,
+					sourceMap: isProduction
+				}
+			}, {
+				loader: 'postcss-loader',
+				options: {
+					ident: 'postcss',
+					plugins: () => [
+						require('postcss-flexbugs-fixes'),
+						require('postcss-preset-env')({
+							autoprefixer: {
+								flexbox: 'no-2009',
+							},
+							stage: 3
+						}),
+						require('css-mqpacker'),
+						require('cssnano')({
+							preset: 'advanced',
+							cssDeclarationSorter: true
+						})
+					],
+					sourceMap: isProduction
+				}
+			}, {
+				loader: 'sass-loader',
+				options: {
+					sourceMap: isProduction,
+					includePaths: [ path.join(__dirname, 'node_modules') ]
+				}
+			}]
+		}, {
+			test: /\.(png|jpe?g|gif|svg)$/,
+			use: [{
+				loader: isProduction ? 'file-loader' : 'url-loader'
 			}, {
 				loader: 'image-webpack-loader',
 				options: {
 					mozjpeg: { progressive: true, quality: 65 },
 					optipng: { enabled: false },
 					pngquant: { quality: '65-90', speed: 4 },
-					gifsicle: { interlaced: false }
-				},
+					gifsicle: { interlaced: false },
+					webp: { quality: 75 }
+				}
 			}]
 		}]
 	},
 
 	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new MiniCssExtractPlugin({
-			filename: isProduction ? 'static/styles/squares.[contenthash:8].css' : 'static/styles/squares.css',
-			chunkFilename: isProduction ? 'static/styles/[id].chunk.[contenthash:8].css' : 'static/styles/[id].chunk.css'
+		new ProgressBarPlugin({
+			format: '\t' + chalk.green.italic(':msg') + '\n\t[' + chalk.green.bold(':bar') + '] ' + chalk.gray(':percent') + ' (:elapsed seconds)',
+			renderThrottle: 100,
+			summary: false,
+			clear: true
 		}),
+		isProduction && new CleanWebpackPlugin([
+			'static',
+			'**/*.html'
+		], {
+			root: BUILD_PATH,
+			verbose: false,
+			beforeEmit: true
+		}),
+		!isProduction && new webpack.HotModuleReplacementPlugin(),
 		new HtmlWebpackPlugin({
 			inject: true,
 			minify: isProduction ? {
@@ -142,13 +147,18 @@ module.exports = {
 				minifyJS: true,
 				minifyCSS: true,
 				minifyURLs: true
-			} : true,
+			} : false,
 			filename: './index.html',
 			template: SOURCE_PATH + '/views/index_template.html'
 		}),
-		new CleanWebpackPlugin([ 'static', '*.html' ], {
-			root: BUILD_PATH,
-			verbose: false
+		isProduction && new MiniCssExtractPlugin({
+			filename: isProduction ? 'static/styles/squares.[contenthash:8].css' : 'static/styles/squares.css',
+			chunkFilename: isProduction ? 'static/styles/[name].[contenthash:8].css' : 'static/styles/[name].chunk.css'
+		}),
+		isProduction && new BundleAnalyzerPlugin({ // report.html
+			analyzerMode: 'static',
+			defaultSizes: 'gzip',
+			openAnalyzer: false
 		})
-	]
+	].filter(Boolean)
 };
