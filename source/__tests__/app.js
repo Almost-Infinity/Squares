2e9ee5d1005a1b3bb908b0fe812c4170f9d7bd76
Alfinity <its.alfinity@gmail.com>
2019-06-01 23:19:18 +0300

    Fixed errors and delete deprecated tests

--- source/__tests__/app.js
+++ source/__tests__/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import App from '../App';
+import App from '../components/app';
 import Header from '../components/header';
 
+/* eslint-disable no-undef*/
 describe('App component', () => {
 	it('should render without throwing an error', () => {
-		expect(shallow(<App />).contains(<Header toggleAuthModal={() => {}} />)).toBe(true);
+		expect(shallow(<App />).contains(<Header />)).toBe(true);
 	});
 });
