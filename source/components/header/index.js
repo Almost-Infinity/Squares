57e2ed4ce20bf2b126aafc7b45366c601679ac44
Alfinity <its.alfinity@gmail.com>
2019-06-22 21:56:16 +0300

    Just commit 2, ahaha. Top fron-end dev

--- source/components/header/index.js
+++ source/components/header/index.js
@@ -1,36 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import style from './styles.sass';
 
-export default class Header extends React.Component {
-	render() {
-		return (
-			<header className={ style.header }>
-				<div className={ style.headerInner }>
-					<div className={ style.headerInnerLeft }>
-						<div className={ style.headerLogo } aria-label="логотип">
-							<a href="/" className={ style.headerLogoInner }></a>
-						</div>
-						<nav className={ style.navbar }>
-							<ul className={ style.navbarItemsWrapper }>
-								<li className={ style.navbarItem }>
-									<Link to="/" className={ style.navbarItemLink }>Главная</Link>
-								</li>
-								<li className={ style.navbarItem }>
-									<Link to="/top" className={ style.navbarItemLink }>Топ игроков</Link>
-								</li>
-								<li className={ style.navbarItem }>
-									<Link to="/blog" className={ style.navbarItemLink }>Блог</Link>
-								</li>
-							</ul>
-						</nav>
+export default function Header() {
+	return (
+		<header className={ style.header }>
+			<div className={ style.headerInner }>
+				<div className={ style.headerInnerLeft }>
+					<div className={ style.headerLogo } aria-label="логотип">
+						<Link to="/" className={ style.headerLogoInner }></Link>
 					</div>
-					<div className={ style.headerInnerMiddle }></div>
-					<div className={ style.headerInnerRight }>
-						<button className={ style.headerBtnSignIn }>Войти</button>
-					</div>
+					<nav className={ style.navbar }>
+						<ul className={ style.navbarItemsWrapper }>
+							<li className={ style.navbarItem }>
+								<Link to="/" className={ style.navbarItemLink }>Главная</Link>
+							</li>
+							<li className={ style.navbarItem }>
+								<Link to="/top" className={ style.navbarItemLink }>Топ игроков</Link>
+							</li>
+							<li className={ style.navbarItem }>
+								<Link to="/blog" className={ style.navbarItemLink }>Блог</Link>
+							</li>
+							<li className={ style.navbarItem }>
+								<Link to="/play" className={ style.navbarItemLink }>Тест</Link>
+							</li>
+						</ul>
+					</nav>
 				</div>
-			</header>
-		);
-	}
+				<div className={ style.headerInnerMiddle }></div>
+				<div className={ style.headerInnerRight }>
+					<Link to="/auth" className={ style.headerBtnSignIn }>Войти</Link>
+				</div>
+			</div>
+		</header>
+	);
 }
