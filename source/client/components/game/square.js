037eec526afa84a530663cf2070756a7c135c6ca
Alfinity <its.alfinity@gmail.com>
2019-09-21 16:04:52 +0300

    Added haveIntersection and haveTouch

--- source/client/components/game/square.js
+++ source/client/components/game/square.js
@@ -1,5 +1,3 @@
-// import { CELL_X, CELL_Y } from './constants';
-
 export default class Square {
   constructor(pos, size, color) {
     this.pos = pos;
@@ -23,5 +21,19 @@ export default class Square {
           console.log('set!');
       }
     }
+  }
+
+  haveIntersection = (anotherX, anotherY, anotherW, anotherH) => {
+    return !(this.pos.x > anotherX + anotherW - 1 ||
+      this.pos.x + this.size.w - 1 < anotherX ||
+      this.pos.y > anotherY + anotherH - 1 ||
+      this.pos.y + this.size.h - 1 < anotherY);
+  }
+
+  haveTouch = (anotherX, anotherY, anotherW, anotherH) => {
+    return !(this.pos.x > anotherX + anotherW ||
+      this.pos.x + this.size.w < anotherX ||
+      this.pos.y > anotherY + anotherH ||
+      this.pos.y + this.size.h < anotherY);
   }
 }
