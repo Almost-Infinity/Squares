22a45f351bdabaa1c15e08d5f8bbd564ac7f7258
Alfinity <its.alfinity@gmail.com>
2020-01-15 10:26:19 +0200

    Added ErrorMessage component

---
+++ source/client/components/ErrorMessage/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Icon from 'Components/Icon';
+import { errorMessageType } from 'Types/props';
+
+import styles from './styles.sass';
+
+function ErrorMessage(props) {
+  const { errorTitle, errorText, reloadBtn = false } = props;
+  return (
+    <div className={styles.errorWrapper}>
+      <div className={styles.errorContainer}>
+        <Icon type='warning' width='80' height='80' className={styles.errorIcon} />
+        <h1 className={styles.errorTitle}>{errorTitle}</h1>
+        <p className={styles.errorText}>{errorText}</p>
+        {
+          reloadBtn && <button className={styles.errorButton} onClick={() => location.reload()}>Перезагрузить</button>
+        }
+      </div>
+    </div>
+  );
+}
+
+ErrorMessage.propTypes = errorMessageType;
+export default ErrorMessage;
--- source/client/types/props.js
+++ source/client/types/props.js
@@ -1,7 +1,13 @@
-import { node, object, func, oneOfType, string } from 'prop-types';
+import { node, object, func, oneOfType, string, bool, number } from 'prop-types';
 
 export const errorBoundaryType = {
   children: node.isRequired
+};
+
+export const errorMessageType = {
+  errorTitle: string.isRequired,
+  errorText: string.isRequired,
+  reloadBtn: bool
 };
 
 export const fieldType = {
@@ -9,11 +15,31 @@ export const fieldType = {
 	addSquare: func.isRequired
 };
 
+// LobbiesList
 export const lobbiesListType = {
   lobbies: object.isRequired,
 	fetchLobbies: func.isRequired
 };
 
+export const lobbiesHeadType = {
+  sortType: string,
+  sortDir: bool,
+  sortLobbiesList: func
+};
+
+export const lobbiesBodyType = {
+  sortType: string,
+  sortDir: bool
+};
+
+export const lobbyItemType = {
+  id: number,
+  name: string,
+  members: number,
+  isPrivate: bool
+};
+
+// Routes
 export const routeExType = {
   component: oneOfType([
     object,
