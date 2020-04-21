4266305cbd820561d2b11458a54d01a959a58c63
Alfinity <its.alfinity@gmail.com>
2020-04-21 22:45:12 +0300

    Add function _cursorToOffscreen

--- source/components/Field/canvas/utilities.js
+++ source/components/Field/canvas/utilities.js
@@ -1,9 +1,44 @@
 import { Canvas } from './core';
 import {
+	CELL_COUNT_X,
+	CELL_COUNT_Y,
+	CELL_SIZE_PX,
 	OFFSCREEN_WIDTH,
 	OFFSCREEN_HEIGHT,
-	SCALE
+	SCALE,
+	FIELD_BORDER
 } from './constants';
+
+Canvas.prototype._createSquare = (x, y, w, h) => ({
+	posX: x,
+	posY: y,
+	width: w,
+	height: h,
+	_beginX: x,
+	_beginY: y
+});
+
+Canvas.prototype._cursorToOffscreen = function(cursorX, cursorY, toCells = false) {
+	let cX = this._viewOffsetX + (cursorX - this._$canvas.offsetLeft - FIELD_BORDER) * this._viewScale;
+	let cY = this._viewOffsetY + (cursorY - this._$canvas.offsetTop - FIELD_BORDER) * this._viewScale;
+
+	if (toCells) {
+		cX = (cX / CELL_SIZE_PX) | 0;
+		cY = (cY / CELL_SIZE_PX) | 0;
+
+		return [
+			cX < 0 ?
+				0 : cX > CELL_COUNT_X ?
+					CELL_COUNT_X - 1 : cX,
+
+			cY < 0 ?
+				0 : cY > CELL_COUNT_Y ?
+					CELL_COUNT_Y - 1 : cY
+		];
+	}
+
+	return [ cX, cY ];
+};
 
 Canvas.prototype._getMaxScale = function() {
 	return this._$canvas.width >= this._$canvas.height ?
